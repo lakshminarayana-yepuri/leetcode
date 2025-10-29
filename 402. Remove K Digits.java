a39e817405b9e4c979b85c4bcbfd426db726f2b4
Lakshmi Narayana Yepuri <79193132+lakshminarayana-yepuri@users.noreply.github.com>
2025-10-29 15:41:53 +0530

    Add solution for removing K digits from a number
    Implement a solution to remove K digits from a number to form the smallest possible number.

---
+++ 402. Remove K Digits.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<num.length();i++){
+            while(k>0 && !st.isEmpty() && st.peek()>num.charAt(i)){
+                st.pop();
+                k--;
+            }
+            st.push(num.charAt(i));
+        }
+        while(k>0){
+            st.pop();
+            k--;
+        }
+        if (st.isEmpty()) return "0";
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+        while (res.length() > 0 && res.charAt(res.length() - 1) == '0') {
+            res.deleteCharAt(res.length() - 1);
+        }
+        res.reverse();
+        if (res.length() == 0) return "0";
+        return res.toString();
+    }
+}
